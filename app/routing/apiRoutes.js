150c0ebb277005632fe9c323b96245da281ca9cd
Dave <davefunk135@gmail.com>
2018-03-01 00:00:36 -0800

    get and post for api routes working. can begin design and logic.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,9 +5,44 @@ var apiApp = express();
 //path for join function
 var path = require('path');
 
+var bodyParser = require('body-parser');
+
+apiApp.use(bodyParser.urlencoded({ extended: false }));
+apiApp.use(bodyParser.json());
+
+var dummyData = [
+    {
+      routeName: "yoda",
+      name: "Yoda",
+      role: "Jedi Master",
+      age: 900,
+      forcePoints: 2000
+    },
+    {
+      routeName: "darthmaul",
+      name: "Darth Maul",
+      role: "Sith Lord",
+      age: 200,
+      forcePoints: 1200
+    },
+    {
+      routeName: "obiwankenobi",
+      name: "Obi Wan Kenobi",
+      role: "Jedi Master",
+      age: 55,
+      forcePoints: 1350
+    }
+  ];
+  
 //get request
-apiApp.get('/api', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/test.html' ));
+apiApp.get('/api/friends', function(req, res){
+    res.json(dummyData);
+})
+apiApp.post('/api/friends', function (req, res) {
+    var friend = req.body;
+    dummyData.push(friend)
+    console.log(friend);
+    res.json(friend)
 });
 
 //export express get 
