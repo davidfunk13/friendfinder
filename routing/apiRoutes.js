6447b81305aee63cc1a7a60e2ed6dc571aff941d
Dave Funk <Dave@Daves-MacBook-Pro.local>
2018-02-28 22:56:25 -0800

    more

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,9 +5,10 @@ var apiApp = express();
 //path for join function
 var path = require('path');
 
-
+//get request
 apiApp.get('/api', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/test.html' ));
 });
 
+//export express get 
 module.exports = apiApp;
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -5,8 +5,10 @@ var htmlApp = express();
 //path for join function
 var path = require('path');
 
+//html endpoint request
 htmlApp.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html' ));
 });
 
+//export html get request
 module.exports = htmlApp;
