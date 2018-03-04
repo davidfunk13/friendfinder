237115a2899a7f9ac52e96df68c69efb85f50f84
Dave <davefunk135@gmail.com>
2018-03-03 18:12:52 -0800

    changin machines

--- app/public/javascript/survey.js
+++ app/public/javascript/survey.js
@@ -1,13 +1,25 @@
-$(document).ready(function (){
-console.log('document ready.')
+$(document).ready(function () {
+    console.log('document ready.')
 
-//button values
-var q1 = $('#q1')
+    //button values
 
-$(document).on('click', '#submit' ,function (){
+    $(document).on('click', '#submit', function () {
 
-console.log(`button is working Q1 val ${q1}`)
-})
+        var surveyValues = {
+
+            q1: $('#q1').val(),
+            q2: $('#q2').val(),
+            q3: $('#q3').val(),
+            q4: $('#q4').val(),
+            q5: $('#q5').val(),
+            q6: $('#q6').val(),
+            q7: $('#q7').val(),
+            q8: $('#q8').val(),
+            q9: $('#q9').val(),
+            q10: $('#q10').val(),
+        }
+    console.log(surveyValues)
+    })
 
 
 });
