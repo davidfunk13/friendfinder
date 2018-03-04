c33224c3eb28fb97500cf49bc535180930d5d2b1
Dave Funk <Dave@daves-mbp.home>
2018-03-03 21:31:11 -0800

    linked friends object

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+let friends = [
+    {
+        name: 'Dave',
+        photo: 'https://imgur.com/TaYObLf',
+        scores: [],
+    },
+    {
+        name: 'Pam',
+        photo: 'https://imgur.com/Pmt9iDZ',
+        scores: [],
+    }
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,12 @@
 var path = require('path');
+let friends = require('../data/friends');
 
-
-var dummyData = [
-    {
-      routeName: "yoda",
-      name: "Yoda",
-      role: "Jedi Master",
-      age: 900,
-      forcePoints: 2000
-    },
-    {
-      routeName: "darthmaul",
-      name: "Darth Maul",
-      role: "Sith Lord",
-      age: 200,
-      forcePoints: 1200
-    },
-    {
-      routeName: "obiwankenobi",
-      name: "Obi Wan Kenobi",
-      role: "Jedi Master",
-      age: 55,
-      forcePoints: 1350
-    }
-  ];
+console.log(friends[0],friends[1])
 
 module.exports = function(app) {
   app.get('/api/friends', function(req, res){
     res.json(dummyData);
-})
+});
 app.post('/api/friend', function (req, res) {
     var friend = req.body;
     dummyData.push(friend)
