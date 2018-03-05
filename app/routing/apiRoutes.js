3fb5230ffaf5453d33f9c5224b0dc3321fefe907
Dave <davefunk135@gmail.com>
2018-03-04 16:47:40 -0800

    reducer added.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,17 +10,34 @@ module.exports = function (app) {
     var userValues = req.body;
     userValuesArray = Object.values(userValues)
     // console.log(`User: ${userValuesArray} Comparisons: ${friends[0].scores} ${friends[1].scores}`);
-    for (var i = 0; i < friends.length; i++){
+    // var differenceGetter = [];
+    for (var i = 0; i < friends.length; i++) {
       console.log(friends[i])
-  
+
       // ////// LOOP 2 STARTS HEREcompare friends[i].scores with userValuesArray//////
-       let friend = friends[i].scores;
-        let you = userValuesArray
-      for (var j = 0; j < 9; j++) {
- ;
+      let friend = friends[i].scores;
+      let you = userValuesArray
+      let sum = 0;        
+      let differenceGetter = [];
+
+      for (var j = 0; j < 10; j++) {
         console.log(`Friend Value: ${friend[j]} You value ${you[j]}`)
+        let valComparison = Math.abs(friend[j] - you[j])
+        console.log(valComparison)
+        differenceGetter.push(valComparison);
+        console.log(differenceGetter)
+        if (differenceGetter.length === 10) {
+          const reducer = (accumulator, currentValue) => accumulator + currentValue;
+          console.log(`total :${differenceGetter.reduce(reducer)}`);
+
+          // for (var k = 0; k < differenceGetter.length; k++) {
+          //   sum += sum )+= parseInt(differenceGetter[i]
+          //   // console.log(differenceGetter[i])
+          // }
+          // console.log(`total${sum}`)
+        }
       }
     }
-      res.json(userValues);
+    res.json(userValues);
   });
 }
