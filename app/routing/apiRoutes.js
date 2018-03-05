93129442265ec875aab058e1995d823499de2d89
Dave <davefunk135@gmail.com>
2018-03-04 16:09:22 -0800

    used i as iterator twice in a nested loop. was less of a headache than i thought it would be fixing only getting one item from friends.length.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,14 +9,16 @@ module.exports = function (app) {
   app.post('/api/friends', function (req, res) {
     var userValues = req.body;
     userValuesArray = Object.values(userValues)
-    console.log(`User: ${userValuesArray} Comparisons: ${friends[0].scores} ${friends[1].scores}`);
+    // console.log(`User: ${userValuesArray} Comparisons: ${friends[0].scores} ${friends[1].scores}`);
     for (var i = 0; i < friends.length; i++){
       console.log(friends[i])
-      //compare friends[i].scores with userValuesArray
-      let friend = friends[i].scores;
-      let you = userValuesArray;
-      for (var i = 0; i < 9; i++) {
-        console.log(`Friend Value: ${friend[i]} You value ${you[i]}`)
+  
+      // ////// LOOP 2 STARTS HEREcompare friends[i].scores with userValuesArray//////
+       let friend = friends[i].scores;
+        let you = userValuesArray
+      for (var j = 0; j < 9; j++) {
+ ;
+        console.log(`Friend Value: ${friend[j]} You value ${you[j]}`)
       }
     }
       res.json(userValues);
